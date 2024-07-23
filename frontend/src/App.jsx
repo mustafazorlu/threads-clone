8997a609a3c4fbb88e349677db0a8a587ffb9747
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-07-23 14:04:44 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Container } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+
+function App() {
+    return (
+        <>
+            <Container maxW={"620px"}>
+                <Routes>
+                    <Route path="/:username" element={<UserPage />} />
+                    <Route path="/:username/post/:pid" element={<PostPage />} />
+                </Routes>
+            </Container>
+        </>
+    );
+}
+
+export default App;
