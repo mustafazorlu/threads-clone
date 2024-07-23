b965a6302d9d3845f38c5501acd8fd93c2241424
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-07-23 19:32:46 +0300

    copy to clipboard

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -1,5 +1,97 @@
+import {
+    Avatar,
+    Box,
+    Flex,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Portal,
+    Text,
+    useToast,
+    VStack,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
 const UserHeader = () => {
-    return <div>UserHeader</div>;
+    const toast = useToast();
+
+    const copyURL = () => {
+        const currentURL = window.location.href;
+        navigator.clipboard.writeText(currentURL).then(() => {
+            toast({ description: "copied" });
+        });
+    };
+    return (
+        <VStack gap={4} alignItems={"start"}>
+            <Flex justifyContent={"space-between"} w={"full"}>
+                <Box>
+                    <Text fontSize={"2xl"} fontWeight={"bold"}>
+                        Mark Zuckerberg
+                    </Text>
+                    <Flex gap={2} alignItems={"center"}>
+                        <Text fontSize={"sm"}>markzuckerberg</Text>
+                        <Text
+                            fontSize={"xs"}
+                            bg={"gray.dark"}
+                            color={"gray.light"}
+                            p={1}
+                            borderRadius={"full"}
+                        >
+                            threads.net
+                        </Text>
+                    </Flex>
+                </Box>
+                <Box>
+                    <Avatar
+                        name="Mark Zuckerberg"
+                        src="/zuck-avatar.webp"
+                        size={"xl"}
+                    />
+                </Box>
+            </Flex>
+            <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, cupiditate!
+            </Text>
+            <Flex w={"full"} justifyContent={"space-between"}>
+                <Flex gap={2} alignItems={"center"}>
+                    <Text color={"gray.light"}>3.2K followers</Text>
+                    <Box
+                        w={1}
+                        h={1}
+                        bg={"gray.light"}
+                        borderRadius={"full"}
+                    ></Box>
+                    <Link color={"gray"}>instagram.com</Link>
+                </Flex>
+                <Flex gap={2} alignItems={"center"}>
+                    <Box className="icon-container" cursor={"pointer"}>
+                        <BsInstagram size={24} cursor={"pointer"} />
+                    </Box>
+                    <Box className="icon-container" cursor={"pointer"}>
+                        <Menu>
+                            <MenuButton>
+                                <CgMoreO size={24} cursor={"pointer"} />
+                            </MenuButton>
+                            <Portal>
+                                <MenuList bg={"gray.dark"}>
+                                    <MenuItem
+                                        bg={"gray.dark"}
+                                        onClick={copyURL}
+                                    >
+                                        Copy Link
+                                    </MenuItem>
+                                </MenuList>
+                            </Portal>
+                        </Menu>
+                    </Box>
+                </Flex>
+            </Flex>
+        </VStack>
+    );
 };
 
 export default UserHeader;
