09808d36e8ffbd32b52cf6c83efb236de63b7f4f
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-07-24 20:46:19 +0300

    post ve user modelleri oluşturuldu

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
 
-dotenv.config();
+dotenv.config(); //dotenv
+connectDB(); //database
 const app = express();
 
+//1:41:21
+
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(5000, () => console.log(`server started at ${PORT}`));
