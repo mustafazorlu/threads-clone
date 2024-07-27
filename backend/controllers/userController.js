b667f659753b1f1167f2440cf1862eb2b885c994
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-07-27 16:35:37 +0300

    takip ve güncelleme sistemi oluşturuldu fakat jwt hatası alıyorum

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -82,35 +82,37 @@ const logoutUser = async (req, res) => {
 const followUnFollowUser = async (req, res) => {
     try {
         const { id } = req.params;
-        const userToModify = await User.findById(id);
-        const currentUser = await User.findById(req.user._id);
+        const userToModify = await User.findById(id); //current user
+        const currentUser = await User.findById(req.user._id); //istek atılan kullanıcı
 
         if (id === req.user._id) {
+            //bu iki değer aynıysa
             return res
                 .status(400)
                 .json({ message: "you cannot follow/unfollow yoursel" });
         }
 
         if (!userToModify || !currentUser) {
+            //kullanıcı yok
             return res.status(400).json({ message: "user not found" });
         }
 
-        const isFollowing = currentUser.following.includes(id);
+        const isFollowing = currentUser.following.includes(id); //şuan ki kullanıcının içerisinde current user id varsa isfallowing true
 
         if (isFollowing) {
-            await User.findByIdAndUpdate(req.user_.id, {
-                $pull: { following: id },
-            });
             await User.findByIdAndUpdate(id, {
                 $pull: { followers: req.user._id },
             });
+            await User.findByIdAndUpdate(req.user_.id, {
+                $pull: { following: id },
+            });
             res.status(200).json({ message: "user unfollowed succesfully" });
         } else {
-            await User.findByIdAndUpdate(req.user_.id, {
-                $push: { following: id },
-            });
             await User.findByIdAndUpdate(id, {
                 $push: { followers: req.user._id },
+            });
+            await User.findByIdAndUpdate(req.user_.id, {
+                $push: { following: id },
             });
             res.status(200).json({ message: "user followed succesfully" });
         }
@@ -119,4 +121,34 @@ const followUnFollowUser = async (req, res) => {
     }
 };
 
-export { signupUser, loginUser, logoutUser, followUnFollowUser };
+const updateUser = async (req, res) => {
+    const { name, email, username, password, profilePic, bio } = req.body;
+    const userId = req.user._id;
+
+    try {
+        let user = await User.findById(userId);
+
+        if (!user)
+            return res.status(400).json({ message: "böyle bir kullanıcı yok" });
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.profilePic = profilePic || user.profilePic;
+        user.bio = bio || user.bio;
+
+        user = await user.save();
+
+        res.status(200).json({ message: "profile updated", user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser };
