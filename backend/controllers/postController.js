5dc9d5241a11c48f8c811a0bfd489f886365b59d
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-07-29 19:33:13 +0300

    like and unlike methods

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -78,4 +78,28 @@ const deletePost = async (req, res) => {
     }
 };
 
-export { createPost, getPost, deletePost };
+const likeUnLikePost = async (req, res) => {
+    try {
+        const { id: postId } = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ message: "post not found" });
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if (userLikedPost) {
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+            res.status(200).json({ message: "post unliked success" });
+        } else {
+            post.likes.push(userId);
+            await post.save()
+            res.status(200).json({ message: "post liked success" });
+        }
+    } catch (error) {}
+};
+
+export { createPost, getPost, deletePost, likeUnLikePost };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,6 +3,7 @@ import {
     createPost,
     getPost,
     deletePost,
+    likeUnLikePost
 } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
+router.post("/like/:id", protectRoute, likeUnLikePost);
 
 export default router;
