0e47cca1632f6a6dda1af59b325b704c774afb8d
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-07-31 19:08:15 +0300

    user atom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
 import "./App.css";
 import { Container } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
 import AuthPage from "./pages/AuthPage";
+import userAtom from "./atoms/userAtom";
+import { useRecoilValue } from "recoil";
+import HomePage from "./pages/HomePage";
 
 function App() {
+    const user = useRecoilValue(userAtom);
     return (
         <>
             <Container maxW={"620px"}>
                 <Header />
                 <Routes>
+                    <Route
+                        path="/"
+                        element={user ? <HomePage /> : <Navigate to={"/auth"} />}
+                    />
                     <Route path="/:username" element={<UserPage />} />
                     <Route path="/auth" element={<AuthPage />} />
                     <Route path="/:username/post/:pid" element={<PostPage />} />
