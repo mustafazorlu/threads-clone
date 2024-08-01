6b88d7de96bab0a84c1e6bd7cfcb8ceb3e82ec44
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-08-01 16:14:26 +0300

    login işlemleri bitti

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -17,10 +17,40 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import authScreenAtom from "../atoms/authAtom";
+import useShowToast from "../hooks/useShowToast";
+import userAtom from "../atoms/userAtom";
 
 const LoginCard = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const setAuthScreen = useSetRecoilState(authScreenAtom)
+    const setAuthScreen = useSetRecoilState(authScreenAtom);
+    const setUser = useSetRecoilState(userAtom);
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: "",
+    });
+    const showToast = useShowToast();
+
+    const handleLogin = async () => {
+        try {
+            const res = await fetch("/api/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(inputs),
+            });
+
+            const data = await res.json();
+
+            if (data.error) {
+                showToast("Error", data.error, "error");
+                return;
+            }
+            console.log(inputs);
+            localStorage.setItem("user-threads", JSON.stringify(data));
+            setUser(data)
+        } catch (error) {
+            showToast("Error", error, "error");
+        }
+    };
     return (
         <Flex align={"center"} justify={"center"}>
             <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
@@ -42,12 +72,30 @@ const LoginCard = () => {
                     <Stack spacing={4}>
                         <FormControl isRequired>
                             <FormLabel>Username</FormLabel>
-                            <Input />
+                            <Input
+                                type="text"
+                                onChange={(e) =>
+                                    setInputs({
+                                        ...inputs,
+                                        username: e.target.value,
+                                    })
+                                }
+                                value={inputs.username}
+                            />
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Password</FormLabel>
                             <InputGroup>
-                                <Input />
+                                <Input
+                                    type={showPassword ? "text" : "password"}
+                                    onChange={(e) =>
+                                        setInputs({
+                                            ...inputs,
+                                            password: e.target.value,
+                                        })
+                                    }
+                                    value={inputs.value}
+                                />
                                 <InputRightElement h={"full"}>
                                     <Button
                                         variant={"ghost"}
@@ -78,6 +126,7 @@ const LoginCard = () => {
                                         "gray.800"
                                     ),
                                 }}
+                                onClick={handleLogin}
                             >
                                 Login
                             </Button>
